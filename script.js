a60bea2c8bc7b364b6c328abed9c3bc2ee8a3cda
colejbodine <colejbodine@gmail.com>
2021-02-16 17:44:18 -0500

    Made dynamic dollar amount. Chart updates on button click.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const myChart = document.getElementById("myChart").getContext('2d');
-
 // Create variables
-let dollarAmount = 100;
-let housing = parseFloat(dollarAmount * 0.25);
-let transportation = parseFloat(dollarAmount * 0.15);
-let groceries = parseFloat(dollarAmount * 0.12);
-let savings = parseFloat(dollarAmount * 0.10);
-let utilities = parseFloat(dollarAmount * 0.10);
-let charity = parseFloat(dollarAmount * 0.05);
-let clothing = parseFloat(dollarAmount * 0.05);
-let entertainment = parseFloat(dollarAmount * 0.05);
-let medical = parseFloat(dollarAmount * 0.05);
-let gifts = parseFloat(dollarAmount * 0.05);
-let misc = parseFloat(dollarAmount * 0.05);
+const myChart = document.getElementById("myChart").getContext("2d");
+const input = document.getElementById("input")
+const update = document.getElementById("update")
 
-let popChart = new Chart(myChart, {
+// Create chart
+function createChart() {
+    let dollarAmount = parseFloat(input.value);
+    let housing = parseFloat(dollarAmount * 0.25);
+    let transportation = parseFloat(dollarAmount * 0.15);
+    let groceries = parseFloat(dollarAmount * 0.12);
+    let savings = parseFloat(dollarAmount * 0.10);
+    let utilities = parseFloat(dollarAmount * 0.10);
+    let charity = parseFloat(dollarAmount * 0.05);
+    let clothing = parseFloat(dollarAmount * 0.05);
+    let entertainment = parseFloat(dollarAmount * 0.05);
+    let medical = parseFloat(dollarAmount * 0.05);
+    let gifts = parseFloat(dollarAmount * 0.05);
+    let misc = parseFloat(dollarAmount * 0.05);
+
+    let popChart = new Chart(myChart, {
     // Types availlable: bar, horizontal, pie, line, doughnut, radar, polarArea    
     type: 'pie',
     data: {
@@ -45,7 +49,19 @@ let popChart = new Chart(myChart, {
                 medical,
                 gifts,
                 misc
-            ]
-        }]
-    }
-});
+                ]
+            }]
+        }
+    });
+
+    return popChart()
+}
+
+function updateChart() {
+    dollarAmount = input.value;
+    createChart();
+}
+
+
+// TODO: Find a way to update/refresh chart on button click with new values.
+update.addEventListener("click", createChart)
